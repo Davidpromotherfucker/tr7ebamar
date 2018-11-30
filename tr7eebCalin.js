b0b4e56295c0eb3affac207ac8f17ffeb811077d
Davidpromotherfucker <44382470+Davidpromotherfucker@users.noreply.github.com>
2018-11-30 16:47:47 +0300

    Add files via upload

---
+++ tr7eebCalin.js
@@ -0,0 +1,23 @@
+const Discord = require('discord.js'),
+Amar = new Discord.Client({sisableEveryone: true})
+Amar.on('guildMemberAdd', member => {
+console.log("Started Welcoming.");
+const Amaar= member.guild.channels.get("517800488637431808");
+if(!Amaar) return;
+if(Amaar) {
+setTimeout(() => Amaar.send('**Wélcome To Cálin.**'), 4500)
+}
+});
+Amar.login("تــوكنك يـآ عَمــــآر");
+
+
+Partly = new Discord.Client({sisableEveryone: true})
+Partly.on('guildMemberAdd', member => {
+console.log("Started Welcoming.");
+const Paartly= member.guild.channels.get("517800488637431808");
+if(!Paartly) return;
+if(Paartly) {
+setTimeout(() => Paartly.send('**Wélcome To Cálin.**'), 4500)
+}
+});
+Partly.login("MzI0MjQ5MjI0OTY5NTg0NjQy.DpgGfw.8vfrznDym3DD8HUcQZk-D8kKafQ");
